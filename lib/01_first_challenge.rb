222a18ffbe5f5fbc189553ec07cd8c28d808aac3
patrick simmons <hadroncollides@gmail.com>
2021-01-17 22:59:50 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -6,6 +6,10 @@ def first_challenge
        :hero 
        :hero_friends => []
      },
-     :capulet => {}
+     :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
    }
 end
